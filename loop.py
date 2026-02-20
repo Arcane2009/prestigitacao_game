ad7d26eb84aa3a9c8d6eef5daf7d94dee4b4e6ff
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-02-20 11:52:06 -0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,40 @@
+#Definição das variáveis#
+total = 5
+turno = 1
+mao_bot = 0
+
+for turno in range(1,total + 1):
+    print(f"Rodada {turno} de {total}")
+    print("\nPedra = 1, papel = 2 e tesoura = 3")
+    entrada = int(input("Digite o número da sua escolha : "))
+
+    if(entrada == 1):
+        print("\nVocê escolheu pedra")
+        mao_bot = mao_bot + 1
+    elif entrada == 2:
+        print("\nVocê escolheu papel")
+        mao_bot = mao_bot + 1
+    else:
+        print("\nVocê escolheu tesoura")
+        mao_bot = mao_bot + 1
+
+    if(mao_bot == 1 and mao == 1):
+        print(f"\nEmpatamos o turno{turno}")
+    elif mao_bot == 2 and mao == 1:
+        print(f"\nGanhei o turno{turno}")
+    elif mao_bot == 3 and mao == 1:
+        print(f"\nPerdi o turno{turno}")
+    elif mao_bot == 1 and mao == 2:
+        print(f"\Perdi o turno{turno}")
+    elif mao_bot == 2 and mao == 2:
+        print(f"\Empatamos o turno{turno}")
+    elif mao_bot == 3 and mao == 2:
+        print(f"\nGanhei o turno{turno}")
+    elif mao_bot == 1 and mao == 3:
+        print(f"\Ganhei o turno{turno}")
+    elif mao_bot == 2 and mao == 3:
+        print(f"\nPerdi o turno{turno}")
+    elif mao_bot == 3 and mao == 3:
+        print(f"\Empatamos o turno{turno}")
+
+print("\nFim de jogo")
